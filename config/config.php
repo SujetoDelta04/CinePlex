b03730bb16a0829e44b488f18f61424577810dfd
Alfredo Melo <alfremelo9@gmail.com>
2024-01-04 20:59:58 +0100

    Se cambia el nombre de conection a confing donde se guardan los datos de conexion a la bd

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return[
+  
+    'database' => [
+        
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'cineplex',
+        'username' => 'root',
+        'password' => '12345678',
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+    ],
+    
+];
+
+
+?>
