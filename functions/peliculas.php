00257c1d5c03454f85823e5e7f0f45b42eef8183
Alfredo Melo <alfremelo9@gmail.com>
2024-01-15 15:45:52 +0100

    Agregadas las funciones sobre detalles de la pelicula

--- functions/peliculas.php
+++ functions/peliculas.php
@@ -4,7 +4,48 @@ require_once '../config/conection.php';
 
 class peliculas_func
 {
-    
+    private $start;
+    private $pdo;
+
+    public function conection()
+    {
+        $this->start=new connection();
+        $this->pdo=$this->start->start_connection();
+        
+        return $this->pdo;
+    }
+    public function show()
+    {
+        $pdo=$this->conection();
+        $sql_s="SELECT peliculas.id, categorias.nombre, titulo, portada FROM peliculas JOIN categorias ON peliculas.categorias_id=categorias.id";
+
+        $show=$pdo->prepare($sql_s);
+        $show->execute();
+        $result=$show->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+    public function show_details($id)
+    {
+        $pdo=$this->conection();
+        $sql_show_details="SELECT * FROM peliculas JOIN categorias ON peliculas.categorias_id=categorias.id WHERE peliculas.id=?";
+
+        $details=$pdo->prepare($sql_show_details);
+        $details->execute(array($id));
+        $show_details=$details->fetchAll(PDO::FETCH_ASSOC);
+
+        return $show_details;
+    }
+}
+
+$functions=new peliculas_func();
+
+if(isset($_GET['id']))
+{
+    $id=$_GET['id'];
+    $details_call=$functions->show_details($id);
+    $details_call_json=json_encode($details_call);
+    header('location: ../public/details.php?packet=' . $details_call_json);
 }
 
 ?>
---
+++ public/details.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | E-commerce</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../vendor/almasaeed2010/adminlte/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <!-- Site wrapper -->
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once 'includes/navbar.php'; ?>
+        <!-- /.navbar -->
+
+        <!-- Slide bar -->
+        <?php require_once 'includes/slider_bar.php'; ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Detalles sobre la pelicula que elegiste</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">E-commerce</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <!-- Default box -->
+                <?php
+                //* Es necesario usar true para que json_decode devuelva un array asociativo
+                $details_show_decode = json_decode($_GET['packet'], true);
+
+                foreach ($details_show_decode as $filas) {
+                ?>
+                    <div class="card card-solid">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12 col-sm-6">
+                                    <h3 class="d-inline-block d-sm-none"></h3>
+                                    <div class="col-12">
+                                        <img src="../multi/pelicullas_portadas/<?php echo $filas['portada']; ?>" class="product-image" alt="Portada pelicula">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <h3 class="my-3"><?php echo $filas['titulo']; ?></h3>
+
+                                    <hr>
+
+                                    <h4 class="mt-3">Puntuacion</h4>
+
+                                    <div class="mt-4">
+                                        <div class="btn btn-primary btn-lg btn-flat">
+                                            <i class="fas fa-cart-plus fa-lg mr-2"></i>
+                                            Comprar Boleta
+                                        </div>
+                                    </div>
+
+                                    <div class="mt-4 product-share">
+                                        <a href="#" class="text-gray">
+                                            <i class="fab fa-facebook-square fa-2x"></i>
+                                        </a>
+                                        <a href="#" class="text-gray">
+                                            <i class="fab fa-twitter-square fa-2x"></i>
+                                        </a>
+                                        <a href="#" class="text-gray">
+                                            <i class="fas fa-envelope-square fa-2x"></i>
+                                        </a>
+                                        <a href="#" class="text-gray">
+                                            <i class="fas fa-rss-square fa-2x"></i>
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div class="row mt-4">
+                                <nav class="w-100">
+                                    <div class="nav nav-tabs" id="product-tab" role="tablist">
+                                        <a class="nav-item nav-link active" id="product-desc-tab" data-toggle="tab" href="#product-desc" role="tab" aria-controls="product-desc" aria-selected="true">Descripcion</a>
+                                        <a class="nav-item nav-link" id="product-comments-tab" data-toggle="tab" href="#product-comments" role="tab" aria-controls="product-comments" aria-selected="false">Comentarios</a>
+                                        <a class="nav-item nav-link" id="product-comments-tab" data-toggle="tab" href="#product-comments" role="tab" aria-controls="product-comments" aria-selected="false">Dejar un comentario</a>
+                                    </div>
+                                </nav>
+                                <div class="tab-content p-3" id="nav-tabContent">
+                                    <div class="tab-pane fade show active" id="product-desc" role="tabpanel" aria-labelledby="product-desc-tab"><?php echo $filas['descripcion']; ?></div>
+                                    <div class="tab-pane fade" id="product-comments" role="tabpanel" aria-labelledby="product-comments-tab"></div>
+                                    <div class="tab-pane fade show active" id="product-desc" role="tabpanel" aria-labelledby="product-desc-tab"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                <?php } ?>
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <footer class="main-footer">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 3.2.0
+            </div>
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        </footer>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="../vendor/almasaeed2010/adminlte/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../vendor/almasaeed2010/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../vendor/almasaeed2010/adminlte/dist/js/demo.js"></script>
+</body>
+
+</html>
